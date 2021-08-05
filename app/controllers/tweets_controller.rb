999a3ce4bd8306ee7ee6a900d8001fc7e81d41e3
Matej Palenik <matejpale@gmail.com>
2021-08-05 14:55:44 +0200

    passed rspec

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,62 @@
 class TweetsController < ApplicationController
 
+    def create
+        @tweet = Tweet.new(tweet_params)
+        token = cookies.permanent.signed[:twitter_session_token]
+        session = Session.find_by(token: token)
+
+        if session
+            user = session.user
+
+            render json: {
+                tweet: {
+                    username: user.username,
+                    message: @tweet.message
+                }
+            }
+        else
+            render json: {
+                success: false
+            }
+        end
+
+    end
+
+    def destroy
+        tweet = Tweet.find_by(id: params[:id])
+        token = cookies.permanent.signed[:twitter_session_token]
+        session = Session.find_by(token: token)
+
+        if session and tweet and tweet.destroy
+            render json: { success: true }
+        else
+            render json: { success: false }
+        end
+    end
+
+    def index
+        @tweets = Tweet.all
+
+        render 'tweets/index'
+    end
+
+    def index_by_user
+        user = User.find_by(username: params[:username])
+        @tweets = user.tweets
+
+        if @tweets
+            render 'tweets/index_by_user'
+        else 
+            render json: {
+                tweets: []
+        }
+        end
+    end
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:message)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: {
+                user: {
+                    username: @user.username,
+                    email: @user.email
+                    
+                }
+            }
+        else
+            render json: {
+                success: false
+            }
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password )
+    end
 
 end
